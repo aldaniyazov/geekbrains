c835e23ffeb7b341afa6fc1430afb9919f164580
aldaniyazov <alda.kanat@gmail.com>
2022-11-25 12:49:14 +0600

    Добавил пример с арифметическими функциями

---
+++ adding/Program.cs
@@ -0,0 +1,20 @@
+﻿// Первый вариант
+int a = 3;
+int b = 5;
+Console.WriteLine(a + b);
+
+// Второй вариант
+int c = 3;
+int d = 5;
+int result = c + d;
+Console.WriteLine(result);
+
+// Деление чисел без остатка
+int numberA = 3;
+int numberB = 5;
+Console.WriteLine(numberA / numberB);
+
+// Деление чисел с остатком
+double numA = 3;
+double numB = 5;
+Console.WriteLine(numA / numB);
