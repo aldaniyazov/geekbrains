dca0f6f9b48226d8162d22f2b5464e49539cbd25
aldaniyazov <alda.kanat@gmail.com>
2022-11-27 22:44:37 +0600

    Решил задание FizzBuzz

---
+++ FIZZ BUZZ/Program.cs
@@ -0,0 +1,19 @@
+﻿int num = new Random().Next(100,1000);
+Console.WriteLine($"Ваше число: {num}");
+
+if ((num % 3 == 0) && (num % 5 == 0))
+{
+	Console.WriteLine("FizzBuzz");
+}
+else if (num % 3 == 0)
+{
+    Console.WriteLine("Fizz");
+}
+else if (num % 5 == 0)
+{
+    Console.WriteLine("Buzz");
+}
+else
+{
+	Console.WriteLine("нет");
+}
