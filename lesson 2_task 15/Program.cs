897105b108929bb16f65a4842a53bec9b13eb99e
aldaniyazov <alda.kanat@gmail.com>
2022-11-27 22:46:19 +0600

    Решение задачи выходных дней

---
+++ lesson 2_task 15/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите день недели: ");
+int day = Convert.ToInt32(Console.ReadLine());
+
+if (day == 6 || day == 7)
+{
+    Console.WriteLine("Выходной день -> да");
+}
+else if (day < 1 || day > 7)
+{
+    Console.WriteLine("Это вообще не день недели");
+}
+else
+{
+    Console.WriteLine("Будний день -> да");
+}
