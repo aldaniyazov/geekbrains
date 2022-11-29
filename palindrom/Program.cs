373831757e4665e8dc7390183186467ea0a3b41f
aldaniyazov <alda.kanat@gmail.com>
2022-11-29 13:43:15 +0600

    Решение задачи № 19

---
+++ palindrom/Program.cs
@@ -0,0 +1,25 @@
+﻿try
+{
+    void PalindromeSearch(int number)
+    {
+        Console.Write("Введите пятизначное число: ");
+        number = Convert.ToInt32(Console.ReadLine());
+
+        if (number.ToString()[0] == number.ToString()[4] && number.ToString()[1] == number.ToString()[3])
+        {
+            Console.WriteLine("Число является палиндромом");
+        }
+        else
+        {
+            Console.WriteLine("Число не является палиндромом");
+        }
+    }
+
+    int n = 0;
+
+    PalindromeSearch(n);
+}
+catch
+{
+    Console.WriteLine("Надо было вводить именно число");
+}
