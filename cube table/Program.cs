a0cd37d24aae3b53c7b93b525ac08d4262ee7498
aldaniyazov <alda.kanat@gmail.com>
2022-12-01 12:29:59 +0600

    Решение задачи № 23

---
+++ cube table/Program.cs
@@ -0,0 +1,22 @@
+﻿void EnterNum(int[] number)
+{
+    for (int i = 0; i < number.Length; i++)
+    {
+        number[i] = i + 1;
+    }
+}
+
+void Cube(int[] number)
+{   
+    for (int i = 0; i < number.Length; i++)
+    {
+        Console.Write($"{Math.Pow(number[i], 3)} ");
+    }
+}
+
+Console.Write("Enter the number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[a];
+
+EnterNum(array);
+Cube(array);
