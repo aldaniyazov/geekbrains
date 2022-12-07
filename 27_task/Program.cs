a75b4ffbf86613a97fba692aa1eb455762c60aad
aldaniyazov <alda.kanat@gmail.com>
2022-12-07 12:44:46 +0600

    Добавил решение задачи № 27

---
+++ 27_task/Program.cs
@@ -0,0 +1,18 @@
+﻿void SumDig(int number)
+{
+    Console.Write("Enter the number: ");
+    number = Convert.ToInt32(Console.ReadLine());
+    int sum = 0;
+    int remainder = 0;
+    while (number > 0)
+    {
+        remainder = number % 10;
+        number = number / 10;
+        sum = sum + remainder;
+    }
+    Console.WriteLine($"Sum of digits in number = {sum}");
+}
+
+int n = 0;
+
+SumDig(n);
