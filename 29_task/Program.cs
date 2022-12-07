ed55728fc637158ed365362b0aa7b497ef8da418
aldaniyazov <alda.kanat@gmail.com>
2022-12-07 16:41:02 +0600

    Добавил решение задачи № 29

---
+++ 29_task/Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Введите 8 целых чисел через пробел:");
+        static void Arr()
+        {
+            string s = Console.ReadLine();
+            string[] line = s.Split(' ');
+            int[] array = new int[line.Length];
+            Console.Write("[");
+            for (int i = 0; i < line.Length - 1; i++)
+            {
+                array[i] = int.Parse(line[i]);
+                Console.Write($"{array[i]}, ");
+            }
+            for (int i = line.Length - 1; i < line.Length; i++)
+            {
+                array[i] = int.Parse(line[i]);
+                Console.Write($"{array[i]}]");
+            }
+        }
+        Arr();
+    }
+}
