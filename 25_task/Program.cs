20ffd75ee3120d9fd78c7323b7c1df11c31a54fa
aldaniyazov <alda.kanat@gmail.com>
2022-12-07 12:41:27 +0600

    Добавлена задача № 25

---
+++ 25_task/Program.cs
@@ -0,0 +1,20 @@
+﻿void Exponent(int a, int b)
+{
+    Console.Write("Enter number A: ");
+    a = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Enter number B: ");
+    b = Convert.ToInt32(Console.ReadLine());
+
+    int result = 1;
+
+    for (int i = 0; i < b; i++)
+    {
+        result = result * a;
+    }
+    Console.Write($"A to the power of B is = {result}");
+}
+
+int a = 0, b = 0;
+
+Exponent(a, b);
