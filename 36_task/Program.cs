8b653f4efce857d8857a8f8d8f0db666f356bd4b
aldaniyazov <alda.kanat@gmail.com>
2022-12-11 15:10:00 +0600

    Добавил решение задачи № 36

---
+++ 36_task/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введи число элементов в массиве:");
+int sum = 0;
+void SumNumbers()
+{
+    int size = Convert.ToInt32(Console.ReadLine());
+    int[] array = new int[size];
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(-1000, 1000);
+    foreach (int el in array)
+        Console.Write($"{el} ");
+    Console.WriteLine();
+
+    for (int i = 0; i < array.Length; i++)
+        if (i % 2 != 0)
+        {
+            sum = sum + array[i];
+        }
+    Console.WriteLine($"Сумма элементов массива, стоящих в нечётных позициях ={sum}");
+}
+SumNumbers();
