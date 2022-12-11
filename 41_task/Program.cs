0627515758dc102d9fe4f6280eb460e02fab9829
aldaniyazov <alda.kanat@gmail.com>
2022-12-11 16:33:18 +0600

    Добавил решение задачи № 41

---
+++ 41_task/Program.cs
@@ -0,0 +1,21 @@
+﻿void AmountMoreZero(int[] array)
+{   
+    int count = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count = count + 1;
+        } 
+    }
+    Console.WriteLine($"User entered {count} numbers greater than zero");
+}
+
+int[] numbers;
+
+Console.Write("Enter numbers separated by spaces: ");
+numbers = Console.ReadLine().Split(' ')
+.Select(e => Convert.ToInt32(e)).ToArray();
+
+AmountMoreZero(numbers);
