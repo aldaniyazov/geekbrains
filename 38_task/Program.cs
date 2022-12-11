f87f071a3858c7561f7bce52d3919c684c7a445b
aldaniyazov <alda.kanat@gmail.com>
2022-12-11 15:14:48 +0600

    Добавил решение задачи № 38

---
+++ 38_task/Program.cs
@@ -0,0 +1,45 @@
+﻿void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i = i + 2)
+    {
+        array[i] = new Random().Next(21, 101);
+    }
+
+    for (int i = 1; i < array.Length; i = i + 2)
+    {
+        array[i] = new Random().Next(1, 21);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+void DiffMinMax(int[] array)
+{
+    int min = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] < min) min = array[i];
+    }
+
+    int max = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] > max) max = array[i];
+    }
+
+    int diff = max - min;
+    Console.WriteLine();
+    Console.WriteLine($"The difference between the maximum and minimum element is {diff}");
+}
+
+int[] a = new int[6];
+
+FillArray(a);
+PrintArray(a);
+DiffMinMax(a);
