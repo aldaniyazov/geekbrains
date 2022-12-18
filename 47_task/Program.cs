3f005c3ba164b735af10e16c5996c4fa2cf6eb28
aldaniyazov <alda.kanat@gmail.com>
2022-12-18 10:32:46 +0600

    Добавил решение задачи № 47

---
+++ 47_task/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Введите количество строк m:");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов n:");
+int n = Convert.ToInt32(Console.ReadLine());
+double[,] array = new double[m,n];
+void FillArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+        array[i,j] = Convert.ToDouble(new Random().Next(-100,100))/10;
+}
+void PrintArray(double[,] array)
+{
+   for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        Console.Write($"{array[i,j]}\t");
+    Console.WriteLine();
+    }
+}
+FillArray(array);
+PrintArray(array);
