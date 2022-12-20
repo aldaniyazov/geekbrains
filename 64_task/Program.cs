798adb38d1e44b9e40f12f3b041733ab03e9c831
aldaniyazov <alda.kanat@gmail.com>
2022-12-20 10:14:05 +0600

    Добавил решение задачи № 64

---
+++ 64_task/Program.cs
@@ -0,0 +1,37 @@
+﻿void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+void FillArray(int[] array, int firstNum)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = firstNum + i;
+    }
+}
+
+void Start()
+{
+    Console.Write("Enter M value: ");
+    int m = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Enter N value: ");
+    int n = Convert.ToInt32(Console.ReadLine());
+
+    if (n < m)
+    {
+        Console.WriteLine("The value of N must be greater than M.");
+        return;
+    }
+
+    int[] numbers = new int[n - m + 1];
+
+    FillArray(numbers, m);
+    PrintArray(numbers);
+}
+
+Start();
